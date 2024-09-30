b23ecfa3f17cf5a2ed1f541ef926e5e5280fd6f1
n3waz <lhk@cesar.school>
2024-09-30 16:58:18 -0300

    created on 'RepositorioEntidadeOperadora' methods

--- src/br/com/cesarschool/poo/titulos/repositorios/RepositorioEntidadeOperadora.java
+++ src/br/com/cesarschool/poo/titulos/repositorios/RepositorioEntidadeOperadora.java
@@ -1,4 +1,12 @@
 package br.com.cesarschool.poo.titulos.repositorios;
+
+import br.com.cesarschool.poo.titulos.entidades.Acao;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Deve gravar em e ler de um arquivo texto chamado Acao.txt os dados dos objetos do tipo
  * Acao. Seguem abaixo exemplos de linhas.
@@ -7,21 +15,110 @@ package br.com.cesarschool.poo.titulos.repositorios;
     2;BANCO DO BRASIL;2026-01-01;21.21
     3;CORREIOS;2027-11-11;6.12 
  * 
- * A inclusão deve adicionar uma nova linha ao arquivo. Não é permitido incluir 
- * identificador repetido. Neste caso, o método deve retornar false. Inclusão com 
+ * A inclusï¿½o deve adicionar uma nova linha ao arquivo. Nï¿½o ï¿½ permitido incluir 
+ * identificador repetido. Neste caso, o mï¿½todo deve retornar false. Inclusï¿½o com 
  * sucesso, retorno true.
  * 
- * A alteração deve substituir a linha atual por uma nova linha. A linha deve ser 
- * localizada por identificador que, quando não encontrado, enseja retorno false. 
- * Alteração com sucesso, retorno true.  
+ * A alteraï¿½ï¿½o deve substituir a linha atual por uma nova linha. A linha deve ser 
+ * localizada por identificador que, quando nï¿½o encontrado, enseja retorno false. 
+ * Alteraï¿½ï¿½o com sucesso, retorno true.  
  *   
- * A exclusão deve apagar a linha atual do arquivo. A linha deve ser 
- * localizada por identificador que, quando não encontrado, enseja retorno false. 
- * Exclusão com sucesso, retorno true.
+ * A exclusï¿½o deve apagar a linha atual do arquivo. A linha deve ser 
+ * localizada por identificador que, quando nï¿½o encontrado, enseja retorno false. 
+ * Exclusï¿½o com sucesso, retorno true.
  * 
  * A busca deve localizar uma linha por identificador, materializar e retornar um 
- * objeto. Caso o identificador não seja encontrado no arquivo, retornar null.   
+ * objeto. Caso o identificador nï¿½o seja encontrado no arquivo, retornar null.   
  */
 public class RepositorioEntidadeOperadora {
 
+    private static final String FILE_PATH = "Acao.txt";
+    private List<Acao> lerArquivo() throws IOException {
+
+        List<Acao> acoes = new ArrayList<>();
+        try (BufferedReader leitor = new BufferedReader(new FileReader(FILE_PATH))) {
+
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+
+                String[] dados = linha.split(";");
+
+                int identificador = Integer.parseInt(dados[0]);
+                String nome = dados[1];
+                LocalDate dataLocal = LocalDate.parse(dados[2]);
+                double valorUnitario = Double.parseDouble(dados[3]);
+
+                acoes.add(new Acao(identificador, nome, dataLocal, valorUnitario));
+            }
+        }
+        return acoes;
+    }
+
+    public void gravarArquivo(List<Acao> acoes) throws IOException {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            for (Acao acao : acoes) {
+
+                writer.write(acao.getIdentificador() + ";"
+                        + acao.getNome() + ";"
+                        + acao.getDataDeValidade() + ";"
+                        + acao.getValorUnitario() + ";");
+                writer.newLine();
+            }
+        }
+    }
+
+    public boolean AlterarAcao(Acao acaoAlterada) throws IOException {
+
+        List<Acao> acoes = lerArquivo();
+        boolean encontrou = false;
+
+        for (int i = 0; i < acoes.size(); i++) {
+            if (acoes.get(i).getIdentificador() == acaoAlterada.getIdentificador()) {
+
+                acoes.set(i, acaoAlterada);
+                encontrou = true;
+                break;
+            }
+        }
+
+        if(encontrou) {
+            gravarArquivo(acoes);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean excluirAcao(int id) throws IOException {
+
+        List<Acao> acoes = lerArquivo();
+        boolean encontrou = false;
+
+        for (int i = 0; i < acoes.size(); i++) {
+            if (acoes.get(i).getIdentificador() == id) {
+
+                acoes.remove(i);
+                encontrou = true;
+                break;
+            }
+        }
+
+        if(encontrou) {
+            gravarArquivo(acoes);
+            return true;
+        }
+        return false;
+    }
+
+    public Acao buscarAcao(int id) throws IOException {
+
+        List<Acao> acoes = lerArquivo();
+        for (Acao acao : acoes) {
+
+            if (acao.getIdentificador() == id) {
+                return acao;
+            }
+        }
+        return null;
+    }
 }
--- src/br/com/cesarschool/poo/titulos/repositorios/RepositorioTransacao.java
+++ src/br/com/cesarschool/poo/titulos/repositorios/RepositorioTransacao.java
@@ -13,10 +13,10 @@ import br.com.cesarschool.poo.titulos.entidades.Transacao;
  *   002192;BCB;true;0.00;1890220034.0;001112;BOFA;true;12900000210.00;3564234127.0;1;PETROBRAS;2024-12-12;30.33;null;100000.0;2024-01-01 12:22:21 
  *   002192;BCB;false;0.00;1890220034.0;001112;BOFA;true;12900000210.00;3564234127.0;null;3;FRANCA;2027-11-11;2.5;100000.0;2024-01-01 12:22:21
  *
- * A inclusão deve adicionar uma nova linha ao arquivo. 
+ * A inclusï¿½o deve adicionar uma nova linha ao arquivo. 
  * 
- * A busca deve retornar um array de transações cuja entidadeCredito tenha identificador igual ao
- * recebido como parâmetro.  
+ * A busca deve retornar um array de transaï¿½ï¿½es cuja entidadeCredito tenha identificador igual ao
+ * recebido como parï¿½metro.  
  */
 public class RepositorioTransacao {
 	public void incluir(Transacao transacao) {
