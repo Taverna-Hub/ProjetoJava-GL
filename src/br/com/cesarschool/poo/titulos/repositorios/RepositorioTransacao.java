aad195fd94184fda125701bb97076dd582cbdac9
n3waz <lhk@cesar.school>
2024-11-22 16:06:40 -0300

    RepositorioTransacao tests working

--- src/br/com/cesarschool/poo/titulos/repositorios/RepositorioTransacao.java
+++ src/br/com/cesarschool/poo/titulos/repositorios/RepositorioTransacao.java
@@ -1,5 +1,7 @@
 package br.com.cesarschool.poo.titulos.repositorios;
 
+import br.com.cesarschool.poo.daogenerico.DAOSerializadorObjetos;
+import br.com.cesarschool.poo.daogenerico.Entidade;
 import br.com.cesarschool.poo.titulos.entidades.Acao;
 import br.com.cesarschool.poo.titulos.entidades.EntidadeOperadora;
 import br.com.cesarschool.poo.titulos.entidades.TituloDivida;
@@ -15,47 +17,55 @@ import java.util.Scanner;
 
 import static java.lang.String.valueOf;
 
-/*
- * Deve gravar em e ler de um arquivo texto chamado Transacao.txt os dados dos objetos do tipo
- * Transacao. Seguem abaixo exemplos de linhas 
- * De entidadeCredito: identificador, nome, autorizadoAcao, saldoAcao, saldoTituloDivida.
- * De entidadeDebito: identificador, nome, autorizadoAcao, saldoAcao, saldoTituloDivida.
- * De acao: (identificador, nome, dataValidade, valorUnitario) OU null
- * De tituloDivida: (identificador, nome, dataValidade, taxaJuros) OU null.
- * valorOperacao, dataHoraOperacao
- *
- *   002192;BCB;true;0.00;1890220034.0; 001112;BOFA;true;12900000210.00;3564234127.0; 1;PETROBRAS;2024-12-12;30.33; null; 100000.0;2024-01-01 12:22:21
- *   002192;BCB;false;0.00;1890220034.0;001112;BOFA;true;12900000210.00;3564234127.0;null;3;FRANCA;2027-11-11;2.5;100000.0;2024-01-01 12:22:21
- *
- * A inclus�o deve adicionar uma nova linha ao arquivo. 
- * 
- * A busca deve retornar um array de transa��es cuja entidadeCredito tenha identificador igual ao
- * recebido como par�metro.  
- */
 public class RepositorioTransacao extends RepositorioGeral{
+	private final DAOSerializadorObjetos<Transacao> dao;
 
 	public RepositorioTransacao() {
 		super(Transacao.class);
+		this.dao = new DAOSerializadorObjetos<>(Transacao.class);
 	}
 
 	public void incluir(Transacao transacao) {
-
+		if (buscar(String.valueOf(transacao.getIdUnico())) == null) {
+			dao.incluir(transacao);
+		}
 	}
 
 	public Transacao[] buscarPorEntidadeCredora(long identificadorEntidadeCredito) {
-		return null;
+		List<Transacao> resultado = new ArrayList<>();
+		for (Transacao transacao : buscarTodos()) {
+			if (transacao.getEntidadeCredito() != null
+			&& transacao.getEntidadeCredito().getIdentificador() == identificadorEntidadeCredito) {
+						resultado.add(transacao);
+			}
+		}
+		return resultado.toArray(new Transacao[0]);
 	}
 
 	public Transacao[] buscarPorEntidadeDevedora(long identificadorEntidadeDebito) {
-		return null;
+		List<Transacao> resultado = new ArrayList<>();
+		for (Transacao transacao : buscarTodos()) {
+			if (transacao.getEntidadeDebito() != null
+			&& transacao.getEntidadeDebito().getIndentificador() == identificadorEntidadeDebito) {
+				resultado.add(transacao);
+			}
+		}
+		return resultado.toArray(new Transacao[0]);
 	}
 
 	@Override
-	public Class getClasseEntidade() {
+	public Class<?> getClasseEntidade() {
 		return Transacao.class;
 	}
 
-	public Transacao[] buscarTodos () {
-		return null;
+	@Override
+	public Transacao[] buscarTodos() {
+		List<Transacao> todasTransacoes = new ArrayList<>();
+		for (Entidade entidade : dao.buscarTodos()) { // O DAO retorna uma lista de Entidade
+			if (entidade instanceof Transacao) { // Verifica se a entidade é uma Transacao
+				todasTransacoes.add((Transacao) entidade); // Faz o casting seguro
+			}
+		}
+		return todasTransacoes.toArray(new Transacao[0]); // Retorna um array de Transacao
 	}
 }
